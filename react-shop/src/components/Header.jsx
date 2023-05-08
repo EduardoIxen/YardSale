4cf327b7976ca2d4a42a3d1b144f609a2e731fa7
EduardoIxen <tomas-1539@hotmail.com>
2023-05-08 01:19:58 -0600

    [ADD] Mostrar productos seleccionados en el carrito de compras y calcular el total

--- react-shop/src/components/Header.jsx
+++ react-shop/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import Menu from '@components/Menu';
 import AppContext from "../context/AppContext";
+import MyOrder from "../containers/MyOrder";
 import "@styles/Header.scss";
 import iconmenu from "@icons/icon_menu.svg";
 import logo from "@logos/logo_yard_sale.svg";
@@ -8,6 +9,7 @@ import shoping_cart from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const [toggleOrders, setToggleOrders] = useState(false);
   const {state} = useContext(AppContext);
 
   const handleToggle = () => {
@@ -45,13 +47,14 @@ const Header = () => {
           <li className="navbar-email" onClick={handleToggle}>
             hi@example.com
           </li>
-          <li className="navbar-shoping-cart">
+          <li className="navbar-shoping-cart" onClick={() => setToggleOrders(!toggleOrders)}>
             <img src={shoping_cart} alt="" />
             {state.cart.length && state.cart.length  > 0 ? <div>{state.cart.length}</div> : null}
           </li>
         </ul>
       </div>
       {toggle && <Menu/>}
+      {toggleOrders && <MyOrder/>}
     </nav>
   );
 };
